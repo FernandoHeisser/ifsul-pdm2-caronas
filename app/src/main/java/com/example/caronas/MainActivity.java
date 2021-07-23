31f64b89e402fba9addd65ee9205059d36485102
Fernando Heisser <fernando_heisser@dellteam.com>
2021-07-23 15:50:01 -0300

    Third commit

--- app/src/main/java/com/example/caronas/MainActivity.java
+++ app/src/main/java/com/example/caronas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.caronas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -7,15 +8,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextEmail, editTextPassword;
-    Button button;
-    TextView textViewRegister;
+    private EditText editTextEmail, editTextPassword;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewRegister = findViewById(R.id.textViewRegister);
+        textViewRegister.setOnClickListener(view -> {
+            YoYo.with(Techniques.Flash)
+                    .duration(700)
+                    .repeat(1)
+                    .playOn(findViewById(R.id.textViewRegister));
+
+            Intent myIntent = new Intent(view.getContext(), RegisterActivity.class);
+            startActivity(myIntent);
+        });
     }
 }
--- app/src/main/java/com/example/caronas/RegisterActivity.java
+++ app/src/main/java/com/example/caronas/RegisterActivity.java
@@ -1,14 +1,38 @@
 package com.example.caronas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextEmailRegister, editTextPasswordRegister, editTextPhone;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(view -> {
+            YoYo.with(Techniques.Flash)
+                    .duration(700)
+                    .repeat(1)
+                    .playOn(findViewById(R.id.button2));
+
+            //TODO add register code
+
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        });
     }
 }
