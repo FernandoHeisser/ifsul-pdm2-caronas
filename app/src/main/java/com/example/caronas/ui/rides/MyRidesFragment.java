fa429a8d868d07588935d9d59c97fd0d2fa8b57a
Fernando Heisser <fernando_heisser@dellteam.com>
2021-07-31 13:53:18 -0300

    Twentieth-third commit

--- app/src/main/java/com/example/caronas/ui/rides/MyRidesFragment.java
+++ app/src/main/java/com/example/caronas/ui/rides/MyRidesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,11 +12,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.caronas.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyRidesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rides, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(v -> {
+            Toast.makeText(view.getContext(), "Funcionou", Toast.LENGTH_LONG).show();
+        });
 
         RecyclerView recyclerView = view.findViewById(R.id.recyclerview_rides);
         recyclerView.setHasFixedSize(true);
