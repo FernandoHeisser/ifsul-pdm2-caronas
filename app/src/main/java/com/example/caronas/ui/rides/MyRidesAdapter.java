848c1c331dea8590d008add65feb3aaa831037a7
Fernando Heisser <fernando_heisser@dellteam.com>
2021-08-03 20:12:26 -0300

    Twentieth-sixth commit

--- app/src/main/java/com/example/caronas/ui/rides/MyRidesAdapter.java
+++ app/src/main/java/com/example/caronas/ui/rides/MyRidesAdapter.java
@@ -3,11 +3,13 @@ package com.example.caronas.ui.rides;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.caronas.HomeActivity;
@@ -16,6 +18,8 @@ import com.example.caronas.Service;
 import com.example.caronas.models.Offer;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class MyRidesAdapter extends RecyclerView.Adapter<MyRidesViewHolder> {
 
@@ -41,62 +45,65 @@ public class MyRidesAdapter extends RecyclerView.Adapter<MyRidesViewHolder> {
         return new MyRidesViewHolder(view);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull @NotNull MyRidesViewHolder holder, int position) {
-        holder.setMyRide(service.myRides.get(position));
-        holder.buttonMyRideCancel.setOnClickListener(v -> {
+        if(Objects.nonNull(service.myRides.get(position)) && Objects.nonNull(service.myRides.get(position).getId())) {
+            holder.setMyRide(service.myRides.get(position));
+            holder.buttonMyRideCancel.setOnClickListener(v -> {
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-            builder.setTitle("Deletar carona");
-            builder.setMessage("Você tem certeza?");
-            builder.setPositiveButton("SIM", (dialog, which) -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Deletar carona");
+                builder.setMessage("Você tem certeza?");
+                builder.setPositiveButton("SIM", (dialog, which) -> {
 
-                if (service.myRides.get(position).getClass().equals(Offer.class)) {
-                    service.cancelOffer(service.myRides.get(position).getId());
-                } else {
-                    service.cancelRequest(service.myRides.get(position).getId());
-                }
-                service.myRides.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, getItemCount());
-                dialog.dismiss();
+                    if (service.myRides.get(position).getClass().equals(Offer.class)) {
+                        service.cancelOffer(service.myRides.get(position).getId());
+                    } else {
+                        service.cancelRequest(service.myRides.get(position).getId());
+                    }
+                    service.myRides.remove(position);
+                    notifyItemRemoved(position);
+                    notifyItemRangeChanged(position, getItemCount());
+                    dialog.dismiss();
 
+                });
+                builder.setNegativeButton("NÃO", (dialog, which) -> dialog.dismiss());
+                AlertDialog alert = builder.create();
+                alert.show();
             });
-            builder.setNegativeButton("NÃO", (dialog, which) -> dialog.dismiss());
-            AlertDialog alert = builder.create();
-            alert.show();
-        });
 
-        if (service.myRides.get(position).getClass().equals(Offer.class)) {
-            holder.vacancies = ((Offer) service.myRides.get(position)).getAvailable_vacancies();
-            holder.textViewCardTitle.setText("Oferta");
-            holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
-            holder.buttonMyRideAdd.setOnClickListener(v -> {
-                try {
-                    service.addVacancy(service.myRides.get(position).getId());
-                    holder.vacancies++;
-                    holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
-                    ((Offer) service.myRides.get(position)).setAvailable_vacancies(holder.vacancies);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            holder.buttonMyRideRemove.setOnClickListener(v -> {
-                try {
-                    service.removeVacancy(service.myRides.get(position).getId());
-                    holder.vacancies--;
-                    holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
-                    ((Offer) service.myRides.get(position)).setAvailable_vacancies(holder.vacancies);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        } else {
-            holder.textViewCardTitle.setText("Pedido");
-            holder.textViewMyRideVacanciesLabel.setText("");
-            holder.buttonMyRideAdd.setVisibility(View.GONE);
-            holder.buttonMyRideRemove.setVisibility(View.GONE);
+            if (service.myRides.get(position).getClass().equals(Offer.class)) {
+                holder.vacancies = ((Offer) service.myRides.get(position)).getAvailable_vacancies();
+                holder.textViewCardTitle.setText("Oferta");
+                holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
+                holder.buttonMyRideAdd.setOnClickListener(v -> {
+                    try {
+                        service.addVacancy(service.myRides.get(position).getId());
+                        holder.vacancies++;
+                        holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
+                        ((Offer) service.myRides.get(position)).setAvailable_vacancies(holder.vacancies);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                holder.buttonMyRideRemove.setOnClickListener(v -> {
+                    try {
+                        service.removeVacancy(service.myRides.get(position).getId());
+                        holder.vacancies--;
+                        holder.textViewMyRideVacancies.setText(holder.vacancies.toString());
+                        ((Offer) service.myRides.get(position)).setAvailable_vacancies(holder.vacancies);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            } else {
+                holder.textViewCardTitle.setText("Pedido");
+                holder.textViewMyRideVacanciesLabel.setText("");
+                holder.buttonMyRideAdd.setVisibility(View.GONE);
+                holder.buttonMyRideRemove.setVisibility(View.GONE);
+            }
         }
     }
 
